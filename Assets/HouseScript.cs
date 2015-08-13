431ccaace077c0bed89fdaf2ccff53b41cc52d3c
msukenik <m45sukenik@gmail.com>
2015-08-13 23:36:56 +0200

    cc
    pohyb objektu medzi trasou

---
+++ Assets/HouseScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseScript : MonoBehaviour {
+
+	void OnMouseDown() {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/PathFollowerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollowerScript : MonoBehaviour {
+	public Transform[] path;
+	public float speed = 3.0f;
+	public float reachDist = 1.0f;
+	public int currentPoint = 0;
+
+	void Start() {
+	  
+	}
+
+	void Update() {
+		float dist = Vector2.Distance (path [currentPoint].position, transform.position);
+
+		transform.position = Vector2.MoveTowards(transform.position, path [currentPoint].position, Time.deltaTime* speed) ;
+
+		if(dist <= reachDist) {
+			currentPoint++;
+		}
+
+		if(currentPoint >= path.Length) {
+			currentPoint = 0;
+		}
+	}
+}
